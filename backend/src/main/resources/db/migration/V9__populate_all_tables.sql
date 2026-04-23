000873de75f20c3e0fe3661bcf6102d465fe78bd
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-04-23 11:48:32 +0800

    added venue migration'3

--- backend/src/main/resources/db/migration/V9__populate_all_tables.sql
+++ backend/src/main/resources/db/migration/V9__populate_all_tables.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `tutor_applications` (
   `course_program` varchar(255) NOT NULL,
   `created_at` datetime(6) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
-  `experience` tinytext NOT NULL,
+  `experience` text NOT NULL,
   `name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
