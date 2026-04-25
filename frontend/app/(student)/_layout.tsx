1f114a922ef7c28736ccdfb63e1e0e43a83e0ed0
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-04-25 19:26:51 +0800

    student  fixed stacking

---
+++ frontend/app/(student)/_layout.tsx
@@ -0,0 +1,19 @@
+import { Tabs } from "expo-router";
+import BottomNav from "../../components/BottomNav";
+
+export default function StudentLayout() {
+  return (
+    <Tabs
+      tabBar={(props) => <BottomNav {...props} />}
+      screenOptions={{
+        headerShown: false,
+        animation: 'fade', // Use fade instead of swipe for a smoother feel
+      }}
+    >
+      <Tabs.Screen name="homepage" />
+      <Tabs.Screen name="book" />
+      <Tabs.Screen name="messages" />
+      <Tabs.Screen name="feedback" />
+    </Tabs>
+  );
+}
